52de1837d21e377f34c51bcb4f8d12b7a4220493
YousefTaha6700 <tahay6700@gmail.com>
2025-09-27 17:41:14 +0300

    update firbase path file

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./push-notification-key.json");
+const path = require("path");
+
+//const serviceAccount = require("./push-notification-key.json");
+const serviceAccount = require(path.join(
+  __dirname,
+  "push-notification-key.json"
+));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
