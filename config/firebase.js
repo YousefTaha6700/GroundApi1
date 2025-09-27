d9265f18c23c6a8e100f0f2c9ba17b9b4100aef7
YousefTaha6700 <tahay6700@gmail.com>
2025-09-27 17:47:34 +0300

    test firbase variable

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,18 @@
 const admin = require("firebase-admin");
-const path = require("path");
 
-//const serviceAccount = require("./push-notification-key.json");
-const serviceAccount = require(path.join(
-  __dirname,
-  "push-notification-key.json"
-));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
+
+// const admin = require("firebase-admin");
+// const serviceAccount = require("./push-notification-key.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// module.exports = admin;
